cbecbaf4fa6f72a1ba2a15ed09e8a0dd861f4a8d
qryskalyst20 <quddunsnuriman@gmail.com>
2023-08-22 19:01:36 +0800

    :art: Moving title to its own component

---
+++ src/components/AppTitle.jsx
@@ -0,0 +1,15 @@
+export default function AppTitle() {
+  return (
+    <>
+      <h1 className="font-bold italic text-2xl">
+        <span className="text-red-700">Rapi</span>
+        <span className="text-gray-600 dark:text-gray-400">dram</span>
+        <span className="text-blue-700">atic</span>
+      </h1>
+      <p className="font-bold">
+        <span className="text-red-700">20</span>
+        <span className="text-blue-700">23</span>
+      </p>
+    </>
+  );
+}
