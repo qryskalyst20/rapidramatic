6cae34d333ae976bbfad7ac507d6930e8ccb5d06
qryskalyst20 <quddunsnuriman@gmail.com>
2023-08-22 18:57:24 +0800

    :lipstick: Update button UI

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -27,7 +27,7 @@ const ThemeSwitcher = () => {
   return (
     <button
       onClick={handleThemeSwitch}
-      className="w-10 h-10 grid place-items-center p-2 bg-yellow-300 dark:bg-zinc-700 rounded-xl absolute right-5 top-2 hover:scale-110"
+      className="w-10 h-10 grid place-items-center p-2 bg-yellow-300 dark:bg-zinc-700 rounded-xl absolute right-5 top-2 hover:scale-110 active:translate-y-1"
     >
       {theme === "dark" ? <FaMoon /> : <BsSunFill />}
     </button>
