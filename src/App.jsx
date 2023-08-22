d67ce5908bb5b271bd6c87029764fd5dadc99baa
qryskalyst20 <quddusnuriman@gmail.com>
2023-08-22 21:03:44 +0800

    :art: Move the month selector to its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import ThemeSwitcher from "./components/ThemeSwitcher";
 import AppTitle from "./components/AppTitle";
+import { BsGithub } from "react-icons/bs";
+import SelectMonth from "./components/SelectMonth";
 
 function App() {
   return (
     <main className="w-screen h-screen bg-slate-100 dark:bg-zinc-900 flex flex-col p-5 items-center">
       <ThemeSwitcher />
       <AppTitle />
+
+      <SelectMonth />
+
+      <BsGithub
+        size={30}
+        className="absolute bottom-5 hover:cursor-pointer hover:scale-105"
+        onClick={() => window.open("https://github.com/qryskalyst20", "_blank")}
+      />
     </main>
   );
 }
