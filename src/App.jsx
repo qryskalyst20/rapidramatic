0b5d8d0869ec185f302fcf9fa74251d65f18b540
qryskalyst20 <quddusnuriman@gmail.com>
2023-08-22 22:01:59 +0800

    :sparkles: Implements chartjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
 import ThemeSwitcher from "./components/ThemeSwitcher";
 import AppTitle from "./components/AppTitle";
 import { BsGithub } from "react-icons/bs";
 import SelectMonth from "./components/SelectMonth";
+import IssueChart from "./components/IssueChart";
+import UserData from "./components/Data";
 
 function App() {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
   return (
     <main className="w-screen h-screen bg-slate-100 dark:bg-zinc-900 flex flex-col p-5 items-center">
       <ThemeSwitcher />
       <AppTitle />
 
       <SelectMonth />
+      <div style={{ width: 700 }}>
+        <IssueChart chartData={userData} />
+      </div>
 
       <BsGithub
         size={30}
