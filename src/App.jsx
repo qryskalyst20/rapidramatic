1c65e7284adf50d3d3d3e04cdca6847cec36051b
qryskalyst20 <quddunsnuriman@gmail.com>
2023-08-22 17:00:04 +0800

    :tada: Initiate Vite app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-zinc-900 flex items-center justify-center">
+      <p className="text-white">Vite test</p>
+    </div>
+  );
+}
+
+export default App;
