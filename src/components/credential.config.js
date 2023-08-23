054ce7b8c17b1d471cefc31f967289bf78438cb2
qryskalyst20 <quddusnuriman@gmail.com>
2023-08-23 19:36:04 +0800

    :memo: Updates credentials instruction

---
+++ src/components/credential.config.js
@@ -0,0 +1,10 @@
+//create new credential.js file in src/components folder
+//use the following format:
+
+const credentials = {
+  USERNAME: "username",
+  PASSWORD: "<password>",
+  EMAIL: "<email>",
+};
+
+export default credentials;
